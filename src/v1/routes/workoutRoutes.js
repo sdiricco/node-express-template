282e4f32484a3eb8e33f094ad02efd4452ce4e86
Simone <sdiricco.91@gmail.com>
2022-11-19 19:18:30 +0100

    add updates

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -17,6 +17,20 @@ const router = express.Router();
  *         schema:
  *           type: string
  *         description: The mode of a workout
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 10
+ *         description: The paginations's limit
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: The paginations's page
  *     responses:
  *       200:
  *         description: OK
@@ -32,6 +46,18 @@ const router = express.Router();
  *                   type: array 
  *                   items: 
  *                     $ref: "#/components/schemas/Workout"
+ *                 paging:
+ *                   type: object 
+ *                   properties: 
+ *                     total: 
+ *                       type: integer
+ *                       example: 10
+ *                     page: 
+ *                       type: integer
+ *                       example: 1
+ *                     pages: 
+ *                       type: integer
+ *                       example: 5
  *       5XX:
  *         description: FAILED
  *         content:
@@ -51,14 +77,196 @@ const router = express.Router();
  */
 router.get("/", workoutController.getAllWorkouts);
 
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: d8be2362-7b68-4ea4-a1f6-03f8bc4eede7
+ *         description: The workout ID
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router.get("/:workoutId", workoutController.getOneWorkout);
 
 router.get("/:workoutId/records", recordController.getRecordForWorkout);
 
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   post:
+ *     tags:
+ *       - Workouts
+ *     requestBody:
+ *       description: Workout entry
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Workout'
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router.post("/", workoutController.createNewWorkout);
 
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   patch:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: d8be2362-7b68-4ea4-a1f6-03f8bc4eede7
+ *         description: The workout ID
+ *     requestBody:
+ *       description: Workout entry
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Workout'
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router.patch("/:workoutId", workoutController.updateOneWorkout);
 
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   delete:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: d8be2362-7b68-4ea4-a1f6-03f8bc4eede7
+ *         description: The workout ID
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
 module.exports = router;
